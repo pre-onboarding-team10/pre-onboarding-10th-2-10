297c837c9dbb641f0ac527d45c1aa6b44909aeca
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-05-03 07:25:12 +0900

    feat: 검색어 추천 폼 레이아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 const App = () => {
-  return <div className="App">Template</div>;
+  return (
+    <div className="App">
+      <SearchInputForm />
+    </div>
+  );
 };
 
 export default App;
+
+const SearchInputForm = () => {
+  return (
+    <div>
+      <div>
+        <input type="search" />
+        <button type="submit">submit</button>
+      </div>
+      <ul>
+        <li>
+          <button>간세포암</button>
+        </li>
+        <li>
+          <button>간세포암</button>
+        </li>
+        <li>
+          <button>간세포암</button>
+        </li>
+      </ul>
+    </div>
+  );
+};

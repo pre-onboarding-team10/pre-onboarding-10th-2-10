bbcb47d90d5a36e7ed2aeeb444571d8aa782679e
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-05-03 08:21:47 +0900

    refact: 추천 검색어 버튼을 컴포넌트로 추출

--- src/components/SearchInputForm.jsx
+++ src/components/SearchInputForm.jsx
@@ -3,17 +3,6 @@ import { useArrowKeyFocus } from '../hooks/useArrowKeyFocus';
 
 export const SearchInputForm = () => {
   const [focus, setFocus] = useArrowKeyFocus(3);
-  const firstRef = useRef(null);
-  const secondRef = useRef(null);
-  const thirdRef = useRef(null);
-
-  useEffect(() => {
-    if (focus === 0) {
-      firstRef.current.focus();
-    } else if (focus === 1) {
-      secondRef.current.focus();
-    } else if (focus === 2) thirdRef.current.focus();
-  }, [focus]);
 
   return (
     <div>
@@ -22,38 +11,29 @@ export const SearchInputForm = () => {
         <button type="submit">submit</button>
       </div>
       <ul>
-        <li>
-          <button
-            ref={firstRef}
-            tabIndex={0}
-            onClick={() => {
-              setFocus(0);
-            }}
-          >
-            간세포암
-          </button>
-        </li>
-        <li>
-          <button
-            ref={secondRef}
-            onClick={() => {
-              setFocus(1);
-            }}
-          >
-            간세포암
-          </button>
-        </li>
-        <li>
-          <button
-            ref={thirdRef}
-            onClick={() => {
-              setFocus(2);
-            }}
-          >
-            간세포암
-          </button>
-        </li>
+        <SearchWordItem focus={focus === 0} setFocus={() => setFocus(0)} />
+
+        <SearchWordItem focus={focus === 1} setFocus={() => setFocus(1)} />
+        <SearchWordItem focus={focus === 2} setFocus={() => setFocus(2)} />
       </ul>
     </div>
+  );
+};
+
+const SearchWordItem = ({ focus, setFocus }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (focus) {
+      ref.current.focus();
+    }
+  }, [focus]);
+
+  return (
+    <li>
+      <button ref={ref} onClick={setFocus}>
+        간세포암
+      </button>
+    </li>
   );
 };

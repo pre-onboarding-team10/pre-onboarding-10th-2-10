0481f758d3c65dcebb84b3eb37b222c9c5f59492
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-05-03 08:54:41 +0900

    refact: 추천 검색어 리스트를 컴포넌트로 추출

--- src/components/SearchInputForm.jsx
+++ src/components/SearchInputForm.jsx
@@ -2,20 +2,25 @@ import { useArrowKeyFocus } from '../hooks/useArrowKeyFocus';
 import { SearchWordItem } from './SearchWordItem';
 
 export const SearchInputForm = () => {
-  const [focus, setFocus] = useArrowKeyFocus(3);
-
   return (
     <div>
       <div>
         <input type="search" />
         <button type="submit">submit</button>
       </div>
-      <ul>
-        <SearchWordItem focus={focus === 0} setFocus={() => setFocus(0)} />
-
-        <SearchWordItem focus={focus === 1} setFocus={() => setFocus(1)} />
-        <SearchWordItem focus={focus === 2} setFocus={() => setFocus(2)} />
-      </ul>
+      <SearchWordList />
     </div>
+  );
+};
+
+const SearchWordList = () => {
+  const [focus, setFocus] = useArrowKeyFocus(3);
+
+  return (
+    <ul>
+      <SearchWordItem focus={focus === 0} setFocus={() => setFocus(0)} />
+      <SearchWordItem focus={focus === 1} setFocus={() => setFocus(1)} />
+      <SearchWordItem focus={focus === 2} setFocus={() => setFocus(2)} />
+    </ul>
   );
 };

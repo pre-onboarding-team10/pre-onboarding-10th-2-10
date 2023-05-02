66dae41f9190765b633bd0bffb0dc1027435ae7d
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-05-03 07:26:38 +0900

    refact: SearchInputForm 컴포넌트를 별도의 파일로 추출

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { SearchInputForm } from './components/SearchInputForm';
+
 const App = () => {
   return (
     <div className="App">
@@ -7,25 +9,3 @@ const App = () => {
 };
 
 export default App;
-
-const SearchInputForm = () => {
-  return (
-    <div>
-      <div>
-        <input type="search" />
-        <button type="submit">submit</button>
-      </div>
-      <ul>
-        <li>
-          <button>간세포암</button>
-        </li>
-        <li>
-          <button>간세포암</button>
-        </li>
-        <li>
-          <button>간세포암</button>
-        </li>
-      </ul>
-    </div>
-  );
-};
---
+++ src/components/SearchInputForm.jsx
@@ -0,0 +1,21 @@
+export const SearchInputForm = () => {
+  return (
+    <div>
+      <div>
+        <input type="search" />
+        <button type="submit">submit</button>
+      </div>
+      <ul>
+        <li>
+          <button>간세포암</button>
+        </li>
+        <li>
+          <button>간세포암</button>
+        </li>
+        <li>
+          <button>간세포암</button>
+        </li>
+      </ul>
+    </div>
+  );
+};

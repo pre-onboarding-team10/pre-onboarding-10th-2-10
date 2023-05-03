b67f45e34adc1248af907da7c7a62a70e62f91d7
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-05-03 09:59:34 +0900

    fix: 검색어 입력 시 바로 추천 검색어 아이템에 포커싱되는 이슈 해결

--- src/hooks/useArrowKeyFocus.jsx
+++ src/hooks/useArrowKeyFocus.jsx
@@ -3,16 +3,24 @@ import { useCallback, useEffect, useState } from 'react';
 const DOWN_ARROW_KEY_KODE = 40;
 const UP_ARROW_KEY_KODE = 38;
 export const useArrowKeyFocus = (size) => {
-  const [currentFocus, setCurrentFocus] = useState(0);
-
+  const [currentFocus, setCurrentFocus] = useState();
+  console.log(currentFocus);
   const handleKeyDown = useCallback(
     (e) => {
       if (e.keyCode === DOWN_ARROW_KEY_KODE) {
         e.preventDefault();
-        setCurrentFocus(currentFocus === size - 1 ? 0 : currentFocus + 1);
+        if (currentFocus === undefined) {
+          setCurrentFocus(0);
+        } else {
+          setCurrentFocus(currentFocus === size - 1 ? 0 : currentFocus + 1);
+        }
       } else if (e.keyCode === UP_ARROW_KEY_KODE) {
         e.preventDefault();
-        setCurrentFocus(currentFocus === 0 ? size - 1 : currentFocus - 1);
+        if (currentFocus === undefined) {
+          setCurrentFocus(size - 1);
+        } else {
+          setCurrentFocus(currentFocus === 0 ? size - 1 : currentFocus - 1);
+        }
       }
     },
     [size, currentFocus, setCurrentFocus]

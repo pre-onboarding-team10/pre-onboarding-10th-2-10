2ae2bda55873693b4b7ed22b381ec79168911495
rabbit-22 <chlwlal20@gmail.com>
2023-05-04 02:34:19 +0900

    feat: fix cache

--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -2,18 +2,18 @@ import React, { useRef } from 'react';
 
 const SuggestionList = ({ suggestions, focusedIndex, setFocusedIndex }) => {
   const suggestionListRef = useRef(null);
-
   return (
     <ul ref={suggestionListRef}>
-      {suggestions.map((suggestion, index) => (
-        <li
-          key={index}
-          style={index === focusedIndex ? { backgroundColor: '#ccc' } : {}}
-          onClick={() => setFocusedIndex(index)}
-        >
-          {suggestion.name}
-        </li>
-      ))}
+      {suggestions.length > 0 &&
+        suggestions.map((suggestion, index) => (
+          <li
+            key={index}
+            style={index === focusedIndex ? { backgroundColor: '#ccc' } : {}}
+            onClick={() => setFocusedIndex(index)}
+          >
+            {suggestion.name}
+          </li>
+        ))}
     </ul>
   );
 };

631b98e4c07b7b1adeed574d7270fe31bb167d6c
전애지 <aejijeon@jeon-aejis-MacBook-Air.local>
2023-05-03 09:51:30 +0900

    feat: 추천 검색어가 없을 경우 알림 메세지 표출

--- src/components/SearchWordList.jsx
+++ src/components/SearchWordList.jsx
@@ -4,6 +4,8 @@ import { SearchWordItem } from './SearchWordItem';
 export const SearchWordList = ({ searchWords }) => {
   const [focus, setFocus] = useArrowKeyFocus(searchWords.length);
 
+  if (searchWords.length === 0) return <span>추천 검색어가 없습니다.</span>;
+
   return (
     <ul>
       {searchWords.map((searchWord, i) => (

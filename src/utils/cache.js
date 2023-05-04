5bbd1d009eeceff0ed759b772bbf97fb6c5ec28e
hyukzz <ri_chard@naver.com>
2023-05-04 13:03:52 +0900

    feat: localstroage cache api data - 캐싱할 때 만료시간 추가, 테스트를 위해 5초 지정 - api를 호출할 때 캐싱한 데이터가 있으면 해당 데이터 사 - 만료시간마다 localstorage에서 캐싱한 데이터 제거 - 실행한 인터벌 삭제

---
+++ src/utils/cache.js
@@ -0,0 +1,59 @@
+const CACHE_PREFIX = 'cache_';
+const EXPIRATION_TIME_IN_MS = 5000;
+
+const getCacheKey = (key) => {
+  return CACHE_PREFIX + key;
+};
+
+const cacheData = (key, data) => {
+  const cacheEntry = {
+    data,
+    expiresAt: Date.now() + EXPIRATION_TIME_IN_MS,
+  };
+  localStorage.setItem(getCacheKey(key), JSON.stringify(cacheEntry));
+};
+
+const getFromCache = (key) => {
+  const cacheEntry = localStorage.getItem(getCacheKey(key));
+  if (cacheEntry) {
+    const { data, expiresAt } = JSON.parse(cacheEntry);
+    if (Date.now() < expiresAt) {
+      return data;
+    } else {
+      localStorage.removeItem(getCacheKey(key));
+    }
+  }
+
+  return null;
+};
+
+const startCacheCleanUpInterval = () => {
+  const cleanUpIntervalId = setInterval(() => {
+    const now = Date.now();
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key.startsWith(CACHE_PREFIX)) {
+        const cacheEntry = localStorage.getItem(key);
+        if (cacheEntry) {
+          const { expiresAt } = JSON.parse(cacheEntry);
+          if (now >= expiresAt) {
+            localStorage.removeItem(key);
+          }
+        }
+      }
+    }
+  }, EXPIRATION_TIME_IN_MS);
+
+  return cleanUpIntervalId;
+};
+
+const stopCacheCleanUpInterval = (intervalId) => {
+  clearInterval(intervalId);
+};
+
+export {
+  cacheData,
+  getFromCache,
+  startCacheCleanUpInterval,
+  stopCacheCleanUpInterval,
+};

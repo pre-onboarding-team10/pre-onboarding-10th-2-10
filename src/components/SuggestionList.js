e77203ec604a70952745ec2e664a09e43e9fbb45
hyukzz <ri_chard@naver.com>
2023-05-04 18:39:46 +0900

    feat: 추천검색어 화면 포커싱 기능 추가 - 최대 7개의 검색어를 보여줌 - 8번 째 검색어부터는 최대 7개의 검색어 비율을 유지하며 포커싱되고, 마지막 검색어에서 keydown시 1번 째 검색어로 이동 - 검색어를 사용하고 다른 검색어를 다시 검색할 때 포커싱 index 값이 고정되어 포커싱위치가 초기화되지 않는 문제해결

--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -1,20 +1,45 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import SuggestionItem from './SuggestionItem';
+import { MAX_SUGGESTIONS } from '../constants/constants';
 
 const SuggestionList = ({ suggestions, focusedIndex, setFocusedIndex }) => {
   const suggestionListRef = useRef(null);
 
+  const startIndex = Math.max(0, focusedIndex - MAX_SUGGESTIONS + 1);
+
+  const renderedSuggestions = suggestions.slice(
+    startIndex,
+    startIndex + MAX_SUGGESTIONS
+  );
+
+  useEffect(() => {
+    setFocusedIndex(-1);
+  }, [suggestions, setFocusedIndex]);
+
   return (
-    <ul ref={suggestionListRef}>
-      {suggestions.map((suggestion, index) => (
-        <li
-          key={index}
-          style={index === focusedIndex ? { backgroundColor: '#ccc' } : {}}
-          onClick={() => setFocusedIndex(index)}
-        >
-          {suggestion.name}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul ref={suggestionListRef}>
+        {suggestions.length <= MAX_SUGGESTIONS
+          ? suggestions.map((suggestion, index) => (
+              <SuggestionItem
+                key={index}
+                index={index}
+                focusedIndex={focusedIndex}
+                setFocusedIndex={setFocusedIndex}
+                suggestionName={suggestion.name}
+              />
+            ))
+          : renderedSuggestions.map((suggestion, index) => (
+              <SuggestionItem
+                key={index}
+                index={startIndex + index}
+                focusedIndex={focusedIndex}
+                setFocusedIndex={setFocusedIndex}
+                suggestionName={suggestion.name}
+              />
+            ))}
+      </ul>
+    </>
   );
 };
 

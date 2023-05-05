e1b6fc53fb1c4db6da7f54ade44b17e08921c151
yan9vvoojin <yan9vvoojin@naver.com>
2023-05-05 14:15:12 +0900

    style: 타이틀 문구 추가

--- src/pages/SearchBar.js
+++ src/pages/SearchBar.js
@@ -19,6 +19,11 @@ const SearchBar = () => {
 
   return (
     <div className="search">
+      <h2>
+        국내 모든 임상시험 검색하고
+        <br />
+        온라인으로 참여하기
+      </h2>
       <SearchBarInput
         keyword={keyword}
         handleInputChange={handleInputChange}
